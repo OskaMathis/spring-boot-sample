5ff4cb91f85a9499cadde7557323c924eb5f540b
OskaMathis <oskamathis@gmail.com>
2020-07-05 00:32:48 +0900

    add logback config

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
+    implementation("net.logstash.logback:logstash-logback-encoder:6.4")
 }
 
 tasks.withType<Test> {
